caa6dbdad88c8b33a49c2d13174680992c239c54
beaulieu527 <beaulieudev@gmail.com>
2019-08-17 13:29:47 -0400

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -24,7 +24,11 @@ class GifListContainer extends Component {
         fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
           .then(res => res.json())
           .then(({data}) => {
-            this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+            this.setState({ 
+                gifs: data.map(
+                     gif => ({ url: gif.images.original.url }) 
+                ) 
+            })
           })
     }
     componentDidMount() {
